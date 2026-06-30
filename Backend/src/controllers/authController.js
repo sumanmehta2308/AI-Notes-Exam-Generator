13d62de890fb9a395e0ca74e46da44d358be8e78
sumanmehta2308 <sumanmehta8298@gmail.com>
2026-06-30 17:12:20 +0530

    production cors and cookie setup

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -19,7 +19,7 @@ export const googleAuth = async (req, res) => {
     const cookieOptions = {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     };
 
